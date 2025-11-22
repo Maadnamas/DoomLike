47d8e365f0d33cab50ce59c3eb28b012b360dace
ELyowiHD <130187343+ELyowiHD@users.noreply.github.com>
2025-11-21 22:04:15 -0300

    decoracion al fondo

---
+++ Assets/Scripts/LightFireFlicker.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightFireHybridFlicker : MonoBehaviour
+{
+    [Header("Intensity Range")]
+    [SerializeField] private float minIntensity = 0.8f;
+    [SerializeField] private float maxIntensity = 1.2f;
+
+    [Header("PingPong Motion")]
+    [SerializeField] private float pingPongSpeed = 2f;
+
+    [Header("Random Flicker")]
+    [SerializeField] private float randomStrength = 0.1f;
+    [SerializeField] private float noiseSpeed = 2f;
+
+    private Light pointLight;
+    private float noiseOffset;
+
+    private void Awake()
+    {
+        pointLight = GetComponent<Light>();
+        noiseOffset = Random.Range(0f, 999f);
+    }
+
+    private void Update()
+    {
+        // --- Ping Pong base ---
+        float pingPong = Mathf.PingPong(Time.time * pingPongSpeed, 1f);
+        float baseIntensity = Mathf.Lerp(minIntensity, maxIntensity, pingPong);
+
+        // --- Ruido Perlin suave ---
+        float noise = (Mathf.PerlinNoise(Time.time * noiseSpeed, noiseOffset) - 0.5f) * randomStrength;
+
+        // --- Resultado final ---
+        pointLight.intensity = baseIntensity + noise;
+    }
+}
